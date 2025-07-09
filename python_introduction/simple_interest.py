cd97d1088139131cdbe94a2932eae4ad427c7cbe
annemutahi <ms.nyamburamutahi@gmail.com>
2025-07-09 15:06:41 +0300

    Added a simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000 # represents $1000
+rate = 0.05 # representing 5% annual interest rate
+time = 3 # in years
+
+interest = principal * rate * time
+
+print('The simple interest is:', interest)
