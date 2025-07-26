aa7e646821af9c78e04e9ae19c15d4f543f5b86f
annemutahi <ms.nyamburamutahi@gmail.com>
2025-07-26 09:37:26 +0300

    used elif instead of match case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,33 @@
+# def perform_operation(num1, num2, operation):
+#     match operation:
+#         case "add":
+#             result = num1+num2
+#         case "subtract":
+#             result = num1-num2
+#         case "multiply":
+#             result = num1*num2
+#         case "divide":
+#             if num2 == 0:
+#                 result = "Zero division is not allowed."
+#             else:
+#                 result = num1/num2
+#         case _:
+#             raise "invalid input."
+#     return result
+
+
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            result = num1+num2
-        case "subtract":
-            result = num1-num2
-        case "multiply":
-            result = num1*num2
-        case "divide":
-            if num2 == 0:
-                result = "Zero division is not allowed."
-            else:
-                result = num1/num2
-        case _:
-            raise "invalid input."
+    if operation == "add":
+        result = num1 + num2
+    elif operation == "subtract":
+        result = num1 - num2
+    elif operation == "multiply":
+        result = num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            result = "Zero division is not allowed."
+        else:
+            result = num1 / num2
+    else:
+        raise ValueError("Invalid input.")
     return result
