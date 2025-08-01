1810e251041fc7b246714fdd7253c01b031f9157
annemutahi <ms.nyamburamutahi@gmail.com>
2025-08-01 12:57:50 +0300

    Added a bank manager program

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return f"Deposited: {amount}. New balance: {self.account_balance}."
+        else:
+            return "Deposit amount must be positive."
+        
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.account_balance:
+                self.account_balance -= amount
+                return f"Withdrew: {amount}. New balance: {self.account_balance}."
+            else:
+                return "Insufficient funds for this withdrawal."
+        else:
+            return "Withdrawal amount must be positive."
+        
+    def display_balance(self):
+        return f"Current balance: {self.account_balance}."
+    
+        
