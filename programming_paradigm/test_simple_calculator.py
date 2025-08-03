40c3cfd70c37b17cbdc0ea97875b25a84d4b9e4a
annemutahi <ms.nyamburamutahi@gmail.com>
2025-08-03 21:54:33 +0300

    changed test_add to test_addition

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
 
     def test_subtract(self):
