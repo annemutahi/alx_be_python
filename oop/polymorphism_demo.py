0618fd7ab5c349385dba64d949009268cbf1a538
annemutahi <ms.nyamburamutahi@gmail.com>
2025-08-10 17:01:46 +0300

    made changes in the ractangle class, added length and width

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -3,9 +3,9 @@ class Shape:
         raise NotImplementedError("Subclasses must implement this method")
     
 class Rectangle(Shape):
-    def __init__(self, width: float, height: float):
+    def __init__(self, length: float, width: float):
+        self.length = length
         self.width = width
-        self.height = height
 
     def area(self) -> float:
         return self.width * self.height
