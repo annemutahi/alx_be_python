5f3d52f51facff6e5c8b6ecfa49be2efef3dc1e3
annemutahi <ms.nyamburamutahi@gmail.com>
2025-08-10 17:03:03 +0300

    made changes

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -8,7 +8,7 @@ class Rectangle(Shape):
         self.width = width
 
     def area(self) -> float:
-        return self.width * self.height
+        return self.length * self.width
     
 class Circle(Shape):
     def __init__(self, radius: float):
